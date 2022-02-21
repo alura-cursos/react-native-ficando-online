e22031bd9cbab75d6878a6713956a32962986ad2
andreocunha <andreoliveiracunha20@gmail.com>
2022-02-21 18:27:43 -0300

    Aula 4.2

--- src/paginas/InfoRepositorio/index.js
+++ src/paginas/InfoRepositorio/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import estilos from './estilos';
-import { salvarRepositoriosDoUsuario } from '../../servicos/requisicoes/repositorios';
+import { salvarRepositoriosDoUsuario, deletarRepositorioDoUsuario } from '../../servicos/requisicoes/repositorios';
 
 export default function InfoRepositorio({ route, navigation }) {
     const [nome, setNome] = useState(route.params.item.name);
@@ -22,7 +22,18 @@ export default function InfoRepositorio({ route, navigation }) {
         else {
             Alert.alert('Erro')
         }
+    }
 
+    async function deletar(){
+        const resultado = await deletarRepositorioDoUsuario(route.params.item.id)
+        
+        if( resultado === 'sucesso'){
+            Alert.alert('Repositorio deletado!')
+            navigation.goBack();
+        }
+        else {
+            Alert.alert('Erro ao deletar o repositorio')
+        }
     }
 
     return (
@@ -51,6 +62,7 @@ export default function InfoRepositorio({ route, navigation }) {
             </TouchableOpacity>
             <TouchableOpacity 
                 style={[estilos.botao, {backgroundColor: '#DD2B2B', marginTop: 10}]} 
+                onPress={deletar}
             >
                 <Text style={estilos.textoBotao}>
                     Deletar
--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -40,3 +40,15 @@ export async function criarRepositoriosDoUsuario(postId, nome, data){
         return 'erro'
     }
 }
+
+
+export async function deletarRepositorioDoUsuario(id){
+    try {
+        await api.delete(`/repos/${id}`);
+        return 'sucesso'
+    }
+    catch (error){
+        console.log(error)
+        return 'erro'
+    }
+}
